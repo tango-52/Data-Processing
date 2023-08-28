df82739cfb38797496238a27bd05b937d771b13e
chinasoft.j.tang <chinasoft.j.tang@jusda.int>
2023-08-28 16:26:56 +0800

    增加写入原有文件

--- tdocdata.py
+++ tdocdata.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from openpyxl import load_workbook
 
-# 读取tdoc数据格式处理文件
+# 读取TDOC数据格式处理文件
 file_path = '数据文件/TDOC数据格式.xlsx'
 df = pd.read_excel(file_path)
 
@@ -8,26 +9,44 @@ df = pd.read_excel(file_path)
 position = df.iloc[1, 14]  # 获取B/L No位置信息
 length = df.iloc[2, 14]  # 获取B/L No截取长度
 
+print(type(position))
+print(position)
+print(type(length))
+
 # 获取原始数据
 origin_data = df.iloc[9, 0]
-print(type(origin_data))
-print(origin_data)
 
 # 切片获取需要替换的子串值
 initial_value = origin_data[position-1:position+length-1]
 
 # 需要替换的值
-replace_value = 't2345678901234567890123'
+replace_value = '828'
 if len(replace_value) <= length:
     replace_fix_value = replace_value.ljust(length)
 else:
     replace_fix_value = replace_value[:length]
 
-print(len(replace_fix_value))
-print(replace_fix_value)
 
 # 将原有的子串值进行替换
 modified_data = origin_data[:position-1] + replace_fix_value + origin_data[position-1+length:]
-print(modified_data)
+
+# 将处理后的数据写入原始数据所在位置
+df.iloc[9, 0] = modified_data
 
 
+# 将修改后的数据写入原始数据所在位置
+df.iloc[9, 0] = modified_data
+
+# 使用openpyxl来写入数据，并保留原始的公式或函数
+wb = load_workbook('数据文件/TDOC数据格式.xlsx')
+ws = wb.active
+
+# 获取修改后的数据
+modified_value = df.iloc[9, 0]
+
+# 将修改后的数据写入到Excel文件中的对应单元格
+cell = ws.cell(row=10, column=1)
+cell.value = modified_value
+
+# 保存工作簿
+wb.save(file_path)
