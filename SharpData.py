635569501a34a42e13c1f78137b3f0ec84de177c
chinasoft.j.tang <chinasoft.j.tang@jusda.int>
2023-09-27 10:04:26 +0800

    处理多组数据打印

--- SharpData.py
+++ SharpData.py
@@ -9,25 +9,22 @@ df = read_excel_file(file_path)  # 读取需要处理的文件
 
 # origin_data = df.iloc[9, 0]  # 获取原始数据
 
-result_positions, result_lengths, replace_values = conf_data(config_data)  # 获取positions、lengths
 
+result_positions, result_lengths, replace_values, data_locations = conf_data(config_data)  # 获取positions、lengths
 
-# 打印修改数据
-def fix_val():
-    for positions_list, lengths_list, replace_value_list in zip(result_positions, result_lengths, replace_values):
-        for position, length, replace_value in zip(positions_list, lengths_list, replace_value_list):
-            print(f'{position}-{length}-{replace_value}')
-            # 更新变量以保存当前组合的值
-            last_position = position
-            last_length = length
-            last_replace_value = replace_value
-    # 在循环结束后，返回最后一个组合的值
-    return last_position, last_length, last_replace_value
+# 将坐标和数据组合在一起
+data_and_coordinates = zip(data_locations, result_positions, result_lengths, replace_values)
 
+# 转换为列表
+data_and_coordinates_list = list(data_and_coordinates)
 
+for (loc, row), positions_list, lengths_list, replace_value_list in data_and_coordinates_list:
+    # 获取原始数据
+    origin_data = df.iloc[loc, row]
+    # print(origin_data)
+    for position, length, replace_value in zip(positions_list, lengths_list, replace_value_list):
+        modified_data = process_data(position, length, replace_value, origin_data)
 
-    # modified_data = process_data(position, length, replace_value, origin_data)
+print(modified_data)
 
     # rewrite_excel(file_path, modified_data)
-
-# print(modified_data)
